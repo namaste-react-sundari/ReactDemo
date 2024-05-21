ae2bd43d1ee986a8acf2fa02549902a0784105f5
“[mk <thejokermaker@gmail.com>
2024-05-20 23:40:30 -0500

    Created Mock data and constants in separate files.

---
+++ src/utils/constant.js
@@ -0,0 +1,7 @@
+export const CDN_LOGO_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ61mn-ENOT3CmofBeorQdW8lJZxU7o80tjrA&s";
+
+export const LOGO_URL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ61mn-ENOT3CmofBeorQdW8lJZxU7o80tjrA&s";
+
+const mk = "mk";
+
+export default mk;
